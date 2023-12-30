47c04e281ba0b5105f3d27bb7808ba658de3dc0f
ozymas <sagarkandel2058@gmail.com>
2023-12-30 21:51:35 +0545

    Updates

--- static/js/8.10cfff4d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpundefined=this.webpackJsonpundefined||[]).push([[8],{67:function(e,n,t){"use strict";t.r(n);t(0),t(69);var s=t(1);n.default=function(e){const{title:n}=e;return Object(s.jsx)("div",{className:"header",children:n})}}}]);
-//# sourceMappingURL=8.10cfff4d.chunk.js.map
